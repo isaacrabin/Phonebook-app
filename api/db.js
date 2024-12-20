a40bbb80c25e04fd8df3895ba0a499a688ba239f
isaacrabin <isaacmrongo@gmail.com>
2024-12-20 22:59:35 +0300

    minor ui updates

--- api/db.js
+++ api/db.js
@@ -1,11 +1,10 @@
-const path = require("path");
-const jsonServer = require("json-server");
-
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "../db.json")); // Adjust path as needed
+const router = jsonServer.router('db.json'); // Path to your JSON data file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
-
-module.exports = server;
+server.use('/api', router); // Prefix all routes with /api
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
