a18cbb53d764ae990f8546dd436cfbc36ee2821b
isaacrabin <isaacmrongo@gmail.com>
2024-12-20 21:35:28 +0300

    added json server dependency

--- api/db.js
+++ api/db.js
@@ -1,15 +1,11 @@
-// api/db.js
-const jsonServer = require('json-server');
+const path = require("path");
+const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // path to your db.json file
+const router = jsonServer.router(path.join(__dirname, "../db.json")); // Adjust path as needed
 const middlewares = jsonServer.defaults();
 
-// Set up middlewares (logging, CORS, etc.)
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
+server.use(router);
 
-// Use the router for the mock API
-server.use('/api', router); // Your API will be accessible at /api
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
+module.exports = server;
