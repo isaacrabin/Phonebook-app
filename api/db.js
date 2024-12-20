1f9f630c37685d8a6d580a847cea61c254c58dcf
isaacrabin <isaacmrongo@gmail.com>
2024-12-20 21:51:19 +0300

    added json server dependency

--- api/db.js
+++ api/db.js
@@ -1,12 +1,11 @@
+const path = require("path");
 const jsonServer = require("json-server");
 
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "../db.json")); // Adjust path as needed
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = (req, res) => {
-  server(req, res);
-};
+module.exports = server;
