a0720906d9d54a5bba8ecdb2ac31c2ac35c968ff
isaacrabin <isaacmrongo@gmail.com>
2024-12-21 05:42:53 +0300

    Fix json-server deployment

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3001' }));
 
 // Mock data (replace this with your JSON file or database connection)
 const  contacts = [
