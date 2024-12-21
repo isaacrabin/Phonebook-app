d75d65e0c8afdc5967515768afd1c10d7df77490
isaacrabin <isaacmrongo@gmail.com>
2024-12-21 03:05:09 +0300

    Fix json-server deployment

--- api/db.js
+++ api/db.js
@@ -1,10 +1,11 @@
-const jsonServer = require('json-server');
+const path = require("path");
+const jsonServer = require("json-server");
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Path to your JSON file
+const router = jsonServer.router(path.join(__dirname, "../db.json")); // Adjust path as needed
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
 module.exports = server;
-
